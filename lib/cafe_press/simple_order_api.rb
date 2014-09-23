1be700788a2b9cbe852057e85e78c34ef1b55f9e
Saurabh Purnaye <spurnaye@gmail.com>
2014-09-23 19:41:03 -0400

    adding ?wsdl to the end point

--- lib/cafe_press/simple_order_api.rb
+++ lib/cafe_press/simple_order_api.rb
@@ -1,7 +1,7 @@
 module CafePress
   module SimpleOrderAPI
     VERSION = '0.0.1'
-    TEST_ENDPOINT = 'http://50.16.203.38/SimpleCPOrderAPIWebServiceSandbox/SimpleCPOrderWS.asmx'
-    LIVE_ENDPOINT = 'https://api.cpdcprod.com/SimpleCPOrderWebService/SimpleCPOrderWS.asmx'
+    TEST_ENDPOINT = 'http://50.16.203.38/SimpleCPOrderAPIWebServiceSandbox/SimpleCPOrderWS.asmx?wsdl'
+    LIVE_ENDPOINT = 'https://api.cpdcprod.com/SimpleCPOrderWebService/SimpleCPOrderWS.asmx?wsdl'
   end
 end
