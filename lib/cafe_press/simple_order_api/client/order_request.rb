7bc064a5729a871f9db74ab62d9d7d890e5677c9
Saurabh Purnaye <spurnaye@gmail.com>
2014-09-23 17:13:44 -0400

    WIP:create order

--- lib/cafe_press/simple_order_api/client/order_request.rb
+++ lib/cafe_press/simple_order_api/client/order_request.rb
@@ -1,9 +1,21 @@
+require 'rexml/document'
+
 module CafePress
   module SimpleOrderAPI
     module Client
       class OrderRequest
-        def create_order(order_id, shipping_address, line_items, options = {})
+        HEADERS = { 'Content-Type' => 'application/xml' }
+        attr_accessor :partner_id, :order, :shipping_address, :line_items, :options
 
+        def initialize(partner_id)
+          @partner_id = partner_id
+        end
+
+        def create_order(order, shipping_address, line_items, options = {})
+          @order = order
+          @shipping_address = shipping_address
+          @line_items = line_items
+          @options = options
         end
 
         def cancel_order(order_id)
@@ -12,6 +24,22 @@ module CafePress
 
         def get_status(order_id)
 
+        end
+
+        protected
+
+        def build_create_order_xml
+          xml = Builder.new
+          xml.instruct!
+          xml.partnerid = @partner_id
+          xml.ord { build_order_xml(xml) }
+          xml.target!
+        end
+
+        def build_order_xml(xml)
+          xml.ord do
+            xml.order_total = @order.
+          end
         end
 
       end # end for class OrderRequest
