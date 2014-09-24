8a296f8d31fe6a56fc1bbe261707d8c281a83209
Saurabh Purnaye <spurnaye@gmail.com>
2014-09-24 17:22:03 -0400

    adding all the required methods for MVP

--- lib/cafe_press/simple_order_api/client.rb
+++ lib/cafe_press/simple_order_api/client.rb
@@ -18,29 +18,42 @@ module CafePress
       end
 
       def create_order(order_hash)
-        @savon_client.call(:create_order, message: order_hash)
+        @savon_client.call(:create_order, message: include_partner_id(order_hash))
       end
 
-      def get_order_status(order_id)
-        @savon_client.call(:create_order, order_hash)
+      def get_order_by_secondary_identifier(identification_hash)
+        @savon_client.call(:get_order_by_secondary_identifier, message: identification_hash)
       end
 
-      def cancel_order(order_id)
-        Client::OrderRequest.cancel_order(order_id)
+      def cancel_order(cafe_press_order_id)
+        hash = {:SalesOrderNo => cafe_press_order_id, :PartnerID => @partner_id }
+        @savon_client.call(:cancel_cp_sales_order, message: hash)
       end
 
-      def get_shipping_info(order_ids, options = {})
-        Client::ShipmentRequest.get_shipping_info(order_ids, options = {})
+      def get_order_status(cafe_press_order_id)
+        hash = {:OrderNo => cafe_press_order_id, :PartnerID => @partner_id }
+        puts client.call(:get_cp_sales_order_status,:message=> hash)
+      end
+
+      def get_shipping_info(cafe_press_order_id)
+        hash = {:SalesOrderNo => cafe_press_order_id, :PartnerID => @partner_id }
+        @savon_client.call(:cancel_cp_sales_order, message: hash)
       end
 
       private
       def end_point(options = {})
-          if options[:live]
-            CafePress::SimpleOrderAPI::LIVE_ENDPOINT
-          else
-            CafePress::SimpleOrderAPI::TEST_ENDPOINT
-          end
+        if options[:live]
+          CafePress::SimpleOrderAPI::LIVE_ENDPOINT
+        else
+          CafePress::SimpleOrderAPI::TEST_ENDPOINT
         end
+      end
+
+      def include_partner_id(hash ={})
+        unless hash.has_key?(:PartnerID)
+          hash[:PartnerID] = @partner_id
+        end
+      end
 
     end # end for class Client
   end # end for module EZP
