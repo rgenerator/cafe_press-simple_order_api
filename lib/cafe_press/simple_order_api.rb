cca3845d82fd289dd737a446c9cb2c1669838f3f
sshaw <skye.shaw@gmail.com>
2014-09-30 21:12:59 -0400

    add error classes

--- lib/cafe_press/simple_order_api.rb
+++ lib/cafe_press/simple_order_api.rb
@@ -1,12 +1,12 @@
-require 'active_utils'
+require 'cafe_press/simple_order_api/client'
+
 module CafePress
   module SimpleOrderAPI
     VERSION = '0.0.1'
 
     root = File.join(File.dirname(__FILE__), 'simple_order_api', 'wsdl')
-    TEST_ENDPOINT = File.join(root, 'sandbox.wsdl')
+    TEST_ENDPOINT = File.join(root, 'sandbox.wsdl').freeze
 
-    # LIVE_ENDPOINT = File.join(root, 'production.wsdl')
-    LIVE_ENDPOINT = 'https://api.cpdcprod.com/SimpleCPOrderWebService/SimpleCPOrderWS.asmx?wsdl'
+    LIVE_ENDPOINT = File.join(root, 'production.wsdl').freeze
   end
 end
